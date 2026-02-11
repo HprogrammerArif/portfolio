3363f81464cee4230b07d84ca9be5adc20becef4
hProgrammerArif <work.mohammedarif@gmail.com>
2026-02-11 17:30:00 +0600

    feat: add portfolio projects configuration and Firebase technology component.

--- src/components/technologies/Sentry.tsx
+++ src/components/technologies/Sentry.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Sanity({ className }: { className?: string }) {
+export default function Sentry({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- src/config/Projects.tsx
+++ src/config/Projects.tsx
@@ -15,7 +15,7 @@ import NodeJs from '@/components/technologies/NodeJs';
 import PostgreSQL from '@/components/technologies/PostgreSQL';
 import Prisma from '@/components/technologies/Prisma';
 import ReactIcon from '@/components/technologies/ReactIcon';
-import Sanity from '@/components/technologies/Sanity';
+import Sentry from '@/components/technologies/Sentry';
 import Shadcn from '@/components/technologies/Shadcn';
 import SocketIo from '@/components/technologies/SocketIo';
 import TailwindCss from '@/components/technologies/TailwindCss';
@@ -40,7 +40,7 @@ export const projects: Project[] = [
 
       { name: 'Expo Go', icon: <Expo key="expo" /> },
       {name: 'Firebase', icon: <Firebase key="firebase" /> },
-      {name: 'Sanity', icon: <Sanity key="sanity" /> },
+      {name: 'Sentry', icon: <Sentry key="Sentry" /> },
     ],
     github: 'https://github.com/HprogrammerArif/co_parenting_update.git',
     live: 'https://co-parenting-app.vercel.app',
@@ -63,7 +63,7 @@ export const projects: Project[] = [
 
       { name: 'Expo Go', icon: <Expo key="expo" /> },
       {name: 'Firebase', icon: <Firebase key="firebase" /> },
-      {name: 'Sanity', icon: <Sanity key="sanity" /> },
+      {name: 'Sentry', icon: <Sentry key="Sentry" /> },
     ],
     github: 'https://github.com/HprogrammerArif/gift_guru.git',
     live: 'https://gift-guru-app.vercel.app',
@@ -122,31 +122,6 @@ export const projects: Project[] = [
 
 
 
-  
-  {
-    title: 'NotesBuddy',
-    description:
-      'A web platform designed for students to read and share notes, study materials and lesson easily. Built with a modern tech stack, it ensures an easy-to-use, visually appealing, and responsive experience.',
-    image: '/project/notesbuddy.png',
-    video: 'https://ik.imagekit.io/hokb3mrdr/notesbuddy.mp4?tr=orig',
-    link: 'https://notesbuddy.in',
-    technologies: [
-      { name: 'Next.js', icon: <NextJs key="nextjs" /> },
-      { name: 'TypeScript', icon: <TypeScript key="typescript" /> },
-      { name: 'React', icon: <ReactIcon key="react" /> },
-      { name: 'Vercel', icon: <Vercel key="vercel" /> },
-      { name: 'MongoDB', icon: <MongoDB key="mongodb" /> },
-      { name: 'Tailwind CSS', icon: <TailwindCss key="tailwindcss" /> },
-      { name: 'Firebase', icon: <Firebase key="firebase" /> },
-      { name: 'shadcn/ui', icon: <Shadcn key="shadcn" /> },
-      { name: 'MDX', icon: <MDXIcon key="mdx" /> },
-    ],
-    github: 'https://github.com/ramxcodes/notesbuddy',
-    live: 'https://notesbuddy.in',
-    details: true,
-    projectDetailsPageSlug: '/projects/notesbuddy',
-    isWorking: true,
-  },
 
 
 
@@ -366,7 +341,7 @@ export const projects: Project[] = [
       { name: 'Next.js', icon: <NextJs key="nextjs" /> },
       { name: 'TypeScript', icon: <TypeScript key="typescript" /> },
       { name: 'Tailwind CSS', icon: <TailwindCss key="tailwindcss" /> },
-      { name: 'Sanity', icon: <Sanity key="sanity" /> },
+      { name: 'Sanity', icon: <Sentry key="sanity" /> },
       { name: 'Vercel', icon: <Vercel key="vercel" /> },
     ],
     github: 'https://github.com/ramxcodes/that-startup',
