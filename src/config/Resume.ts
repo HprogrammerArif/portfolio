b43c6898ad262f5398834479858f9bd54c70a720
hProgrammerArif <work.mohammedarif@gmail.com>
2026-02-14 13:59:33 +0600

    feat: Add resume configuration file with a Google Drive URL.

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,4 +1,4 @@
 export const resumeConfig = {
   // url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
-  url: 'https://drive.google.com/file/d/1wBOYS4UYD_tnLocivMe2PYBfrq-kzxRD/view',
+  url: 'https://drive.google.com/file/d/1wBOYS4UYD_tnLocivMe2PYBfrq-kzxRD/preview',
 };
