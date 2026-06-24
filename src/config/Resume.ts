8d36f11ba2bb956bc8190b2f6978ce95c327dc69
HprogrammerArif <work.mohammedarif@gmail.com>
2026-06-24 22:18:19 +0600

    feat: add resume configuration file and update resume PDF link

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,4 +1,4 @@
 export const resumeConfig = {
   // url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
-  url: 'https://drive.google.com/file/d/1wBOYS4UYD_tnLocivMe2PYBfrq-kzxRD/preview',
+  url: 'https://drive.google.com/file/d/1fp5LAIBuOqSDpXlP75Z7IU6a1WKx4biZ/view?usp=sharing',
 };
